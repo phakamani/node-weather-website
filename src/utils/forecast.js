8fa887835fe4b23079b3926ed7901911b9c05a85
unknown <mkulisep@gmail.com>
2019-11-06 10:53:02 +0200

    Added minimum and maximum temperature

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -9,7 +9,9 @@ const forecast = (latitude, longitude, callback) => {
         } else if (body.error) {
             callback(error, undefined);
         } else {
-            callback(undefined, `${body.daily.data[0].summary} It is currently ${body.currently.temperature} degrees out. There is a ${body.currently.precipProbability}% chance of rain`);
+            let weatherDesscription = `${body.daily.data[0].summary} It is currently ${body.currently.temperature} degrees out. There is a ${body.currently.precipProbability}% chance of rain`;
+            weatherDesscription = `${weatherDesscription}. The minimum daily temperature is ${body.daily.data[0].temperatureLow} degrees and the maximum is ${body.daily.data[0].temperatureHigh} degrees`
+            callback(undefined, weatherDesscription);
         }
     })
 }
